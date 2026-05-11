de6840002b35b3fedc036184e0e022c5812c68e0
Achinta <achintasiddhanta0304@gmail.com>
2026-05-11 21:40:13 +0530

    all routes

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import verifyPhoneRoute from "./phone-verify.route";
+import userRoute from "./user.route";
+import deviceRoute from "./device.route";
+import valveRoute from "./valve.route";
 
 const router = Router();
 
 router.use("/verify-phone", verifyPhoneRoute)
+
+router.use("/users", userRoute)
+
+router.use("/devices", deviceRoute)
+
+router.use("/valve", valveRoute)
 
 export default router;
